7cc7d74c7b047106c4532f89d3b177ca073a903b
Pranav Moorthy <pranavsmoorthy@gmail.com>
2025-01-06 06:49:06 -0500

    added documentation for userRoutesAdmin

--- onboarder/routes/admin/userRoutesAdmin.js
+++ onboarder/routes/admin/userRoutesAdmin.js
@@ -20,6 +20,20 @@ const {
  *             500:
  *                 description: Unable to get list of all users
 */
+/**
+ * @api {get} /api/admin/users List users
+ * @apiName List users
+ * @apiPermission admin
+ * @apiGroup Users (Admin)
+ * @apiDescription Get all users
+ *
+ * @apiHeader {String} token Authentication token.
+ * 
+ * @apiSuccess {Array} users Array of all user objects.
+ * @apiError InternalServerError internal server error
+ * 
+ * @apiVersion 1.0.0
+ */
 router.route('/')
     .get(listUsers);
 
@@ -104,6 +118,64 @@ router.route('/')
  *             500:
  *                 description: Unable to update user
 */
+/**
+ * @api {get} /api/admin/users/:id Get user
+ * @apiName Get user
+ * @apiPermission admin
+ * @apiGroup Users (Admin)
+ * @apiDescription Gets user with given id
+ *
+ * @apiHeader {String} token Authentication token.
+ * 
+ * @apiParam {String} id ID of user.
+ *
+ * @apiSuccess {Object} user User object in given id.
+ * @apiError Unauthorized unauthorized
+ * @apiError NotFound User with given id not found
+ * @apiError InternalServerError internal server error
+ * 
+ * @apiVersion 1.0.0
+ */
+/**
+ * @api {put} /api/admin/users/:id Update user
+ * @apiName Update user
+ * @apiPermission admin
+ * @apiGroup Users (Admin)
+ * @apiDescription Update user details for given id
+ *
+ * @apiHeader {String} token Authentication token.
+ * 
+ * @apiParam {String} id User id.
+ * @apiBody {String} username Updated (or existing) username
+ * @apiBody {String} password Updated (or existing) password
+ * @apiBody {String} email Updated (or existing) email
+ * @apiBody {String} role Updated (or existing) user's role. Possible values are: "Admin" and "User"
+ *
+ * @apiSuccess {Object} user User object.
+ * @apiError Unauthorized unauthorized
+ * @apiError NotFound could not find user with given id
+ * @apiError InternalServerError internal server error
+ * 
+ * @apiVersion 1.0.0
+ */
+/**
+ * @api {delete} /api/admin/users/:id Delete user
+ * @apiName Delete User
+ * @apiPermission admin
+ * @apiGroup Users (Admin)
+ * @apiDescription Delete user with given id
+ *
+ * @apiHeader {String} token Authentication token.
+ * 
+ * @apiParam {String} id User id.
+ *
+ * @apiSuccess {Object} user Deleted user object.
+ * @apiError Unauthorized unauthorized
+ * @apiError NotFound could not find user with given id
+ * @apiError InternalServerError internal server error
+ * 
+ * @apiVersion 1.0.0
+ */
 router.route('/:id')
     .get(getUserForAdminRoute)
     .put(updateUserForAdminRoute)
