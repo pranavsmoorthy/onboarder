1e7b01a988fd2a74d3d45180717d2a0fbeef9f26
Pranav Moorthy <pranavsmoorthy@gmail.com>
2025-01-05 20:04:04 -0500

    added documentation to course routes

--- onboarder/routes/admin/courseRoutes.js
+++ onboarder/routes/admin/courseRoutes.js
@@ -14,6 +14,7 @@ const {
  * @swagger
  * /api/admin/courses:
  *      get:
+ *         summary: Get all courses
  *         description: Get all courses
  *         responses:
  *             200:
@@ -21,28 +22,113 @@ const {
  *             500:
  *                 description: Unable to get list of all courses
  *      post:
+ *          summary: Create course
  *          description: Create a new course
- *          parameters:
- *              - in: body
- *                name: title
- *                type: string
- *                required: true
- *                description: Title of the course
- *              - in: body
- *                name: description
- *                type: string
- *                required: false
- *                description: Description of the course
- *              - in: body
- *                name: title
- *                type: string
- *                required: true
- *                description: Title of the course
+ *          requestBody:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              title:
+ *                                  type: string
+ *                                  required: true
+ *                              link:
+ *                                  type: string
+ *                                  required: true
+ *                              description:
+ *                                  type: string
+ *                                  required: false
+ *                              completionDate:
+ *                                  type: string
+ *                                  required: true
+ *          responses:
+ *              201: 
+ *                  description: Successful response, new course created
+ *              400:
+ *                  description: Bad request
+ *              401:
+ *                  description: Unauthorized
+ *              500: 
+ *                  description: Internal server error
 */
 router.route('/')
     .post(createCourse)
     .get(listCourses);
 
+/** 
+ * @swagger
+ * /api/admin/courses/{id}:
+ *      get:
+ *          summary: Get course by ID
+ *          description: Get course by ID
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                type: string
+ *                required: true
+ *                description: ID of course to get
+ *          responses:
+ *              200:
+ *                  description: Successful response
+ *              401:
+ *                  description: Unauthorized
+ *              404:
+ *                  description: Course with given ID could not be found
+ *              500:
+ *                  description: Unable to get list of all courses
+ *      put:
+ *          summary: Update course by ID
+ *          description: Update course by ID
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                type: string
+ *                required: true
+ *                description: ID of course to update
+ *          requestBody:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              title:
+ *                                  type: string
+ *                                  required: true
+ *                              link:
+ *                                  type: string
+ *                                  required: true
+ *                              description:
+ *                                  type: string
+ *                                  required: false
+ *          responses:
+ *              201: 
+ *                  description: Successful response, new course created
+ *              400:
+ *                  description: Bad request
+ *              401:
+ *                  description: Unauthorized
+ *              500: 
+ *                  description: Internal server error
+ *      delete:
+ *          summary: Delete course by ID
+ *          description: Delete course by ID
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                type: string
+ *                required: true
+ *                description: ID of course to delete
+ *          responses:
+ *              200:
+ *                  description: Successful response
+ *              401:
+ *                  description: Unauthorized
+ *              404:
+ *                  description: Course with given ID could not be found
+ *              500:
+ *                  description: Unable to get delete course
+*/
 router.route('/:id')
     .get(getCourseById)
     .put(updateCourseById)
